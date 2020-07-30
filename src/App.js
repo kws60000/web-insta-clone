bd680dd7b5f837ad38919922d3884813900aeea0
dylan <kws60000@naver.com>
2020-07-30 10:21:04 +0900

    프로젝트의 배경색을 레드로 변경합니다.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
 import Header from "./components/Header";
+import "./App.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </div>
   );
 };
 
